99d426b4fa009f81cf72b8caeee6967773da6b32
Will Campbell <wibb1@comcast.net>
2020-08-07 15:33:12 -0400

    correct errors

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -1,17 +1,5 @@
 class Api::V1::TripsController < ApiController
   def index
-<<<<<<< HEAD
-    binding.pry
-    render json: Trip.all
-    #render json: user, serializer: UserSerializer
-    #binding.pry
-  end
-
-  def show
-    trip = Trip.find(params[:id])
-    #render json: user, serializer: UserSerializer
-=======
     render json: current_user.trips
->>>>>>> master
   end
 end
