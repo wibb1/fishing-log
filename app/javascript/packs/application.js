29ff10ae7f8835235c9cf453910bc8831b656cbd
Will Campbell <wibb1@comcast.net>
2020-08-08 19:29:29 -0400

    Completed navbar burger

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,32 +5,6 @@ import { render } from 'react-dom'
 import App from '../react/components/App'
 import RedBox from 'redbox-react'
 
-document.addEventListener('DOMContentLoaded', function () {
-
-  // Get all "navbar-burger" elements
-  var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-
-  // Check if there are any nav burgers
-  if ($navbarBurgers.length > 0) {
-
-    // Add a click event on each of them
-    $navbarBurgers.forEach(function ($el) {
-      $el.addEventListener('click', function () {
-
-        // Get the target from the "data-target" attribute
-        var target = $el.dataset.target;
-        var $target = document.getElementById(target);
-
-        // Toggle the class on both the "navbar-burger" and the "navbar-menu"
-        $el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
-
-      });
-    });
-  }
-
-});
-
 document.addEventListener('DOMContentLoaded', () => {
   let reactElement = document.getElementById('app')
 
@@ -46,4 +20,25 @@ document.addEventListener('DOMContentLoaded', () => {
       render(<App />, reactElement)
     }
   }
+
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll(`.navbar-burger`), 0);
+
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach( element => {
+      element.addEventListener(`click`, () => {
+
+        // Get the target from the "data-target" attribute
+        const target = element.dataset.target;
+        const $target = document.getElementById(target);
+        debugger
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        element.classList.toggle(`is-active`);
+        $target.classList.toggle(`is-active`);
+      });
+    });
+  };
 })
