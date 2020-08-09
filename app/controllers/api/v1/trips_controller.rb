13e60c2d34d454890fe109a082b6d4e8d920f9fb
Will Campbell <wibb1@comcast.net>
2020-08-09 08:07:59 -0400

    pulled master

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -2,4 +2,9 @@ class Api::V1::TripsController < ApiController
   def index
     render json: current_user.trips
   end
+
+  def show
+    product = Trip.find(params[:id])
+    render json: trip, serializer: Trip
+  end
 end
