7eda85696778de2fed47e789fe17a5ce0fa2d91b
Will Campbell <wibb1@comcast.net>
2020-08-10 15:52:04 -0400

    formatting for Show Page

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import ReactDOM from 'react-dom'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faThumbsUp, faThumbsDown } from '@fortawesome/free-solid-svg-icons'
+
 import TripsIndexContainer from './TripsIndexContainer';
 import TripShowContainer from './TripShowContainer';
+
+library.add(fab, faThumbsUp, faThumbsDown)
 
 export const App = (props) => {
 	return (
