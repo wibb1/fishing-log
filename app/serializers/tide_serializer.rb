a6693e380e4743dd3b5d3e820da49a2d3628b65c
Will Campbell <wibb1@comcast.net>
2020-08-17 18:01:49 -0400

    codeship/heroku errors

--- app/serializers/tide_serializer.rb
+++ app/serializers/tide_serializer.rb
@@ -1,4 +1,3 @@
 class TideSerializer < ActiveModel::Serializer
   attributes :id, :first_type, :first_time, :second_type, :second_time, :third_type, :third_time, :fourth_type, :fourth_time, :date
-
 end
